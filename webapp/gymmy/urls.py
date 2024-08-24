e8b5276003bf2c3722673be7a403853e1c3a2868
Suraj <surajvasan@gmail.com>
2024-08-24 17:24:37 +0800

    Backend for login, register and logout

--- webapp/gymmy/urls.py
+++ webapp/gymmy/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('',views.home, name="homepage"),
     path('about/',views.about, name="about"),
-    path('login/',views.login, name="login"),
-    path('signup/',views.signup, name="signup"),
+    path('login/',views.login_user, name="login"),
+    path('logout/',views.logout_user, name="logout"),
+    path('signup/',views.signup_user, name="signup"),
 ]
--- webapp/gymmy/views.py
+++ webapp/gymmy/views.py
@@ -1,18 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-
-
+from django.contrib.auth import authenticate, login, logout
+from .forms import UserRegisterForm
+from django.contrib import messages
 # Create your views here.
 
-def home(response):
-    return render(response, "gymmy/landing.html", {})
+def home(request):
+    return render(request, "gymmy/landing.html", {})
 
-def about(response):
-    return render(response, 'gymmy/aboutpage.html', {})
+def about(request):
+    return render(request, 'gymmy/aboutpage.html', {})
 
-def login(response):
-    return render(response, 'gymmy/login.html', {})
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username = username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, f'Welcome Back! {username}')
+            return redirect('homepage')
+        else:
+            return redirect('login')
+    else:
+        return render(request, 'gymmy/login.html', {})
 
-def signup(response):
-    return render(response, 'gymmy/signup.html', {})
+def signup_user(request):
+    form = UserRegisterForm(request.POST)
+    if form.is_valid():
+        form.save()
+        return redirect('login')
+    else:
+        form = UserRegisterForm()
 
+    context = {'form':form}
+    return render(request, 'gymmy/signup.html', context) 
+        
+def logout_user(request):
+    logout(request)
+    return redirect('homepage')
