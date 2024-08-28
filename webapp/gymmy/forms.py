5624756370c40999ebac3fbc13975514c9c625fa
LimJianFeng <jflfeng94877@gmail.com>
2024-08-28 19:29:16 +0800

    “ProfileChanges

--- webapp/gymmy/forms.py
+++ webapp/gymmy/forms.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm,PasswordChangeForm
 from django.contrib.auth.models import User
 from django import forms
 from .models import Profile
@@ -11,25 +11,27 @@ class UserRegisterForm(UserCreationForm):
         fields = ['username','email','password1','password2']
 
 class UserUpdateForm(forms.ModelForm):
-    email = forms.EmailField()
     class Meta:
-        model = User 
-        fields = ['username', 'email']
+        model = User
+        fields = ['username']
 
+class PasswordUpdateForm(PasswordChangeForm):
+    class Meta:
+        model = User
+        fields = ['old_password', 'new_password1', 'new_password2']
 
 class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ['image']
-        def save(self, *args, **kwargs):
-            profile = super().save(*args, **kwargs)
 
-            if profile.image:
-                img = Image.open(profile.image.path)
-                if img.height > 300 or img.width > 300:
-                    output_size = (300, 300)
-                    img.thumbnail(output_size)
-                    img.save(profile.image.path)
-
-            return profile
+    def save(self, *args, **kwargs):
+        profile = super().save(*args, **kwargs)
+        if profile.image:
+            img = Image.open(profile.image.path)
+            if img.height > 300 or img.width > 300:
+                output_size = (300, 300)
+                img.thumbnail(output_size)
+                img.save(profile.image.path)
+        return profile
   
