a8ff52bea99685823622000825d281ce2d7e2315
Suraj <surajvasan@gmail.com>
2024-09-15 00:48:22 +0800

    devphase1

--- webapp/gymmy/admin.py
+++ webapp/gymmy/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from .models import Post, Profile, Category, Routines
-from .models import Profile
+from .models import Profile, WorkoutProgress
 
 admin.site.register(Post)
 admin.site.register(Profile)
 admin.site.register(Category)
 admin.site.register(Routines)
-
+admin.site.register(WorkoutProgress)
 from .models import FlexcamPost, Comment
 
 admin.site.register(FlexcamPost)
