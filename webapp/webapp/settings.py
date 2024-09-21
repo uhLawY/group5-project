30ba50ca78d10d03566820b76bbb36f7e96980d7
Suraj <surajvasan@gmail.com>
2024-09-21 20:32:30 +0800

    fixed some bugs

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1264!4tln4=a==oa@wd)9h2l6ttj9e*0#3k(%(%uq2m!7!%7p$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['gymmy.pythonanywhere.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
