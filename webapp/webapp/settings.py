1a1b23a7242dce74bc3a010fed9d819dd2ed5ed9
LimJianFeng <jflfeng94877@gmail.com>
2024-09-28 20:10:47 +0800

    'debug'

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1264!4tln4=a==oa@wd)9h2l6ttj9e*0#3k(%(%uq2m!7!%7p$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['gymmy.pythonanywhere.com', 'localhost', '127.0.0.1']
 
